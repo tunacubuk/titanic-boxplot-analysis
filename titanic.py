99f349849e4efc3b86fab02a2c9c309156bf10f9
Tuna Çubuk <cubuktuna@gmail.com>
2026-05-06 11:10:10 +0300

    Add files via upload

---
+++ titanic.py
@@ -0,0 +1,24 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+df=sns.load_dataset("titanic")
+
+print(df)
+
+plt.subplot(1,2,1)
+sns.boxplot(x="class", y="age", data=df)
+plt.title("Age by Class")
+plt.xlabel("Class")
+plt.ylabel("Age")
+
+
+
+plt.subplot(1,2,2)
+sns.boxplot(x="class", y="fare", data=df)
+plt.title("Fare by Class")
+plt.xlabel("Class")
+plt.ylabel("Fare")
+plt.show()
+
+
